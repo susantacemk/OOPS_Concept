adf0ad6fcc7bdadb10607e230417bbdad053f0e0
susantacemk <cse21083@cemk.ac.in>
2024-07-22 16:48:12 +0000

    classes and object and Wrapper class final and this keyword

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -1,6 +1,56 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hii I am Susanta Maity");
+        // create a dataType where stores the 5 roll No
+        // int[] rollNo = new int[5];
+
+        // create a dataType where stores the 5 students names
+        // String []names=new String[5];
+
+        // create a dataType where stores the 5 students marks
+        // float[] marks=new float[5];
+
+        // Now teachers said to create a data Structure where stores the 5 students
+        // data , data means ={names, roll Nos, marks}
+
+        // To solve that type of problem create a custom class - Students
+
+        // create an object which are namely susanta
+
+        Students susanta = new Students(83, "Susanta Maity", 8.9f);
+
+        // using dot operator (separator) we can access the instance variables and
+        // method
+        // based on their accesibility
+        System.out.println(susanta.mark); // 8.9
+        System.out.println(susanta.name); // Susanta Maity
+
+        Students one = new Students(1, "Sreya das", 7.8f);
+
+        Students two = one;
+        two.name = "Soumili Mondal";
+        System.out.println(two.mark);
+        System.out.println(one.name);
+
     }
+
+    // create a Students class
+    static class Students {
+        // here are three Property
+        int rollNo;
+        String name;
+        float mark;
+
+        // create a constructor
+        // constructor- a special type of method which is used to initialized
+        // the value of class variable during the time of object creation
+
+        public Students(int rollNo, String name, float mark) {
+            this.rollNo = rollNo;
+            this.name = name;
+            this.mark = mark;
+        }
+
+    }
+
 }
---
+++ Introduction/WrapperExample.java
@@ -0,0 +1,57 @@
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        Integer num1 = 57;
+        Integer num2 = 45;
+        // Integer num=new Integer(34); it is deprecated
+
+        // Now our question is what is the difference between int and Integer
+        // int - premitive Type
+        // Integer - non Premitive Type
+
+        swap(a, b);
+        System.out.println(a + " " + b); // cannot swaped - because java methods call pasess the value
+
+        swapNonPremitive(num1, num2);
+
+        System.out.println(num1 + " " + num2); // cannot swaped
+
+        // lets talk about the final keyword
+        final int FINAL_A = 18; // once a variable is fianl; then it cannot be modified..
+        // FINAL_A=90; // this occurs error final cannot be modiafied..
+        System.out.println(FINAL_A);
+
+        final A susanta = new A("Susanta maity");
+        // susanta = new A("Susanta"); // this will give error because susanta is final
+        susanta.name = "Other Name"; // this is allowed because we are not changing the reference of the object
+        System.out.println(susanta.name);
+
+    }
+
+    static void swap(int a, int b) { // premitive type
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void swapNonPremitive(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
+
+    /**
+     * A class
+     */
+    public static class A {
+        final int a = 90;
+        String name;
+
+        public A(String name) {
+            System.out.println("A class is created");
+            this.name = name;
+        }
+    }
+}
